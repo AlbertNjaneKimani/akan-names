e9c080615354d1514b89101a6cb85d89621a1648
Albert Kimani <albert.njane@azenia.com>
2022-11-03 09:10:40 +0300

    add arrays of weekdays,and khan names

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday'];
+const khanMales=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var khanFemales =['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
