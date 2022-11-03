c4228490f48a936bbf48e297ccafd6aebdf84219
Albert Kimani <albert.njane@azenia.com>
2022-11-03 10:49:01 +0300

    add switchcase for getting male names

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday'];
+var CC, YY, MM, DD, d, birthDay;
+const weekDay = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday'];
 const khanMales = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var khanFemales = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-var CC, YY, MM,DD, d, birthDay;
+
 const validateInputs = () => {
     var year = document.getElementById("year").value;
     var month = document.getElementById("month").value;
@@ -31,11 +32,49 @@ const validateInputs = () => {
         return true;
     }
 };
-const calculateDayFromFormular=()=>{
+const calculateDayFromFormular = () => {
     year = document.getElementById("year").value;
-    CC = parseInt(year.substring(0,2));
-    YY = parseInt(year.substring(2,4));
+    CC = parseInt(year.substring(0, 2));
+    YY = parseInt(year.substring(2, 4));
     MM = parseInt(document.getElementById("month").value);
     DD = parseInt(document.getElementById("day").value);
-    d= (((CC/4) -2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+}
+const getGenderName = () => {
+    var genders = document.getElementByname("gender");
+    if (genders[0].checked == true) {
+        var gender = "male";
+    } else if (genders[1].checked == true) {
+        var gender = "female";
+    }
+    else {
+        return false;
+    }
+    switch (gender) {
+        case "male":
+            if (birthDay == 1) {
+                alert("Your Akan name is " + khanMales[0]);
+            }
+            else if (birthDay == 2) {
+                alert("Your Akan name is " + khanMales[2]);
+            }
+            else if (birthDay == 3) {
+                alert("Your Akan name is " + khanMales[3]);
+            }
+            else if (birthDay == 4) {
+                alert("Your Akan name is " + khanMales[4]);
+            }
+            else if (birthDay == 4) {
+                alert("Your Akan name is " + khanMales[4]);
+            }
+            else if (birthDay == 5) {
+                alert("Your Akan name is " + khanMales[5]);
+            }
+            else if (birthDay == 6) {
+                alert("Your Akan name is " + khanMales[6]);
+            }
+            else if (birthDay == 7) {
+                alert("Your Akan name is " + khanMales[7]);
+            }
+    }
 }
